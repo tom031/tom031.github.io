df534541846eef8ad66372743fe240ba24fcb11d
Tom Dinh <tomdinhnz@gmail.com>
2016-11-16 20:49:34 +1300

    Finished Sidenav with button ng-click, and will come back for ng-class for animate the Sidenav

--- js/myApp.js
+++ js/myApp.js
@@ -1,7 +1,11 @@
-var app = angular.module('app', ['ngMaterial']);
+var app = angular.module('app', ['ngMaterial','ngAnimate']);
 app.controller('appCtrl', function ($scope, $mdSidenav) {
-    $scope.hi = "Hello";
-    var hi2 = "hello";
-    console.log(hi2);
-})
-;
+$scope.open = 0;
+var isShowRightContent = false;
+ if (isShowRightContent) {
+   $scope.open =1
+ } else {
+   $scope.open =1
+ }
+ console.log(isShowRightContent);
+});
