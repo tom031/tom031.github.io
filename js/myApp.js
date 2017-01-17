f3484f6107bacd25a830a1d81eed147ac59b843d
Tom Dinh <tomdinhnz@gmail.com>
2017-01-18 12:37:08 +1300

    added directive for slideshow

--- js/myApp.js
+++ js/myApp.js
@@ -1,11 +1,31 @@
-var app = angular.module('app', ['ngMaterial','ngAnimate','ngMdIcons']);
-app.controller('appCtrl', function ($scope, $mdSidenav) {
-$scope.open = 0;
-var isShowRightContent = false;
- if (isShowRightContent) {
-   $scope.open = 1
- } else {
-   $scope.open = 1
- }
- //console.log(isShowRightContent);
+var app = angular.module('app', ['ngMaterial', 'ngAnimate', 'ngMdIcons']);
+app.controller('appCtrl', function($scope, $mdSidenav) {
+});
+app.directive('tabsAutoplay', function($interval) {
+    return {
+        restrict: 'A',
+        require: 'mdTabs',
+        link: function(scope, elm, attrs, mdTabsCtrl) {
+            var defaultDelay = 2000,
+                tabsAutoplayDelay = +attrs.tabsAutoplay || defaultDelay;
+
+            var tabsAutoplayInterval = $interval(function() {
+                var nextTab;
+                for (i = mdTabsCtrl.selectedIndex + 1; i <= mdTabsCtrl.tabs.length; i++) {
+                    nextTab = mdTabsCtrl.tabs[i];
+                    if (nextTab && (nextTab.scope.disabled !== true)) {
+                        return mdTabsCtrl.select(nextTab.getIndex());
+                    };
+                };
+                return mdTabsCtrl.select(0);
+            }, tabsAutoplayDelay);
+
+            var cleanup = function() {
+                $interval.cancel(tabsAutoplayInterval);
+                elm.off('click', cleanup);
+            };
+            elm.on('click', cleanup);
+            scope.$on('$destroy', cleanup);
+        },
+    }
 });
