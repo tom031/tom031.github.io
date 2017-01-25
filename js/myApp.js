c77962e63b815f214225fc1fe44b3c2410c3b728
Tom Dinh <Tom Dinh>
2017-01-25 20:01:36 +1300

    add hosting json and dynamic hostlink

--- js/myApp.js
+++ js/myApp.js
@@ -1,42 +1,77 @@
 var app = angular.module('app', ['ngMaterial', 'ngAnimate', 'ngMdIcons']);
-app.controller('appCtrl', function($scope, $mdSidenav, $log, $mdMedia) {
+//TODO: Get Json File
+app.factory('getData', function ($http) {
+    return {
+        get: function (_url) {
+            var req = {
+                method: 'get',
+                url: _url
+            };
+            return $http(req);
+        }
+    }
+});
+app.controller('appCtrl', function ($scope, $mdSidenav, $log, $mdMedia, getData) {
+    //TODO: hosting link changing
+    $scope.domainname = null;
+    var hosting = document.domain;
+    if (hosting.match(/\b\wgithub/g)) {
+        return $scope.domainname = 'github';
+    }
+    else if (hosting.match(/\b\wazurewebsites/g)) {
+        return $scope.domainname = 'azure';
+    }
+    else if (hosting.match(/\b\waws|\b\wamazone/g)) {
+        return $scope.domainname = 'aws';
+    }
+    else ($scope.domainname = 'localhost');
+
+
+    //TODO: sideNav controlling
     this.isOpen = false;
     this.hover = false;
     var isOpenRight;
-    var domainname;
-    domainname = document.domain;
-    console.log(domainname);
+
     $scope.buttonSidenav = $scope.$watch(
-        function() {
+        function () {
             return $mdMedia('gt-md');
         },
-        function(openOrNot) {
+        function (openOrNot) {
             $scope.buttonSidenav = openOrNot;
         });
-    $scope.openSidenav = function() {
-        $mdSidenav('right').open().then(function(isOpenRight = true) {
+    $scope.openSidenav = function () {
+        $mdSidenav('right').open().then(function (isOpenRight = true) {
             $log.debug("close RIGHT is done");
             console.log("true isOpenRight", isOpenRight);
         });
     };
-    $scope.closeSidenav = function() {
-        $mdSidenav('right').close().then(function(isOpenRight = false) {
+    $scope.closeSidenav = function () {
+        $mdSidenav('right').close().then(function (isOpenRight = false) {
             $log.debug("close RIGHT is done");
             console.log("false isOpenRight", isOpenRight);
             //$scope.isOpenRight = false;
         });
 
     };
+
+    //TODO: Read Json data
+    var hostDataUrl = '/data/hosting.json';
+    getData.get(hostDataUrl).then(function successCallback(response) {
+        $scope.hostData = response.data.hostProviders;
+        //console.log(response.data.hostProviders[0]);
+    }), function (response) {
+        defer.reject('could not find someFile.json');
+    };
 });
-app.directive('tabsAutoplay', function($interval) {
+app.directive('tabsAutoplay', function ($interval) {
     return {
         restrict: 'A,E',
         require: 'mdTabs',
-        link: function(scope, elm, attrs, mdTabsCtrl) {
+        link: function (scope, elm, attrs, mdTabsCtrl) {
             var defaultDelay = 2000,
                 tabsAutoplayDelay = +attrs.tabsAutoplay || defaultDelay;
 
-            var tabsAutoplayInterval = $interval(function() {
+            var tabsAutoplayInterval = $interval(function () {
                 var nextTab;
                 for (i = mdTabsCtrl.selectedIndex + 1; i <= mdTabsCtrl.tabs.length; i++) {
                     nextTab = mdTabsCtrl.tabs[i];
@@ -47,7 +82,7 @@ app.directive('tabsAutoplay', function($interval) {
                 return mdTabsCtrl.select(0);
             }, tabsAutoplayDelay);
 
-            var cleanup = function() {
+            var cleanup = function () {
                 $interval.cancel(tabsAutoplayInterval);
                 elm.off('click', cleanup);
             };
