77b64715d86eb5846055e3866429afee36e70c1b
Tom Dinh <tomdinhnz@gmail.com>
2017-01-26 22:34:37 +1300

    test IIFE (Immediately-invoked function expression)

--- js/myApp.js
+++ js/myApp.js
@@ -1,119 +1,123 @@
-var app = angular.module('app', ['ngMaterial', 'ngAnimate', 'ngMdIcons']);
+(function () {
+    var app = angular.module('app', ['ngMaterial', 'ngAnimate', 'ngMdIcons']);
 
-//TODO: Get Json File
-app.factory('getData', function ($http) {
-    return {
-        get: function (_url) {
-            var req = {
-                method: 'get',
-                url: _url
-            };
-            return $http(req);
+    //TODO: Get Json File
+    app.factory('getData', function ($http) {
+        return {
+            get: function (_url) {
+                var req = {
+                    method: 'get',
+                    url: _url
+                };
+                return $http(req);
+            }
         }
-    }
-});
-
-app.controller('appCtrl', function ($scope, $mdSidenav, $log, $mdMedia, getData) {
-    $scope.$on('$viewContentLoaded', function () {
-        var hostDataUrl = '/jsonfiles/hosting.json';
-        getData.get(hostDataUrl).then(function successCallback(response) {
-            $scope.hostData = response.data;
-            console.log(response.data.hostProviders[0]);
-        }), function (response) {
-            defer.reject('could not find someFile.json');
-            console.log(response.data);
-        };
-        console.log("content loaded",response.data);
     });
 
-    //TODO: hosting link changing
-    $scope.domainname = null;
-    var hosting = document.domain;
-    console.log(hosting);
-    if (hosting.match(/github/gi)) {
-        return $scope.domainname = 'github';
-    }
-    else if (hosting.match(/azure/gi)) {
-        return $scope.domainname = 'azure';
-    }
-    else if (hosting.match(/aws|amazon/gi)) {
-        return $scope.domainname = 'aws';
-    }
-    else ($scope.domainname = 'localhost');
+    app.controller('appCtrl', function ($scope, $mdSidenav, $log, $mdMedia, getData) {
 
-    //TODO: sideNav controlling
-    //screen size variable
-    $scope.screenSize = $scope.$watch(function () {
-        return $mdMedia('(min-width: 1610px)');
-    }, function (trueOrfalse) {
-        return $scope.screenSize = trueOrfalse;
-    })
+        //TODO: hosting link changing
+        $scope.domainname = null;
+        var hosting = document.domain;
+        console.log(hosting);
+        if (hosting.match(/github/gi)) {
+            return $scope.domainname = 'github';
+        }
+        else if (hosting.match(/azure/gi)) {
+            return $scope.domainname = 'azure';
+        }
+        else if (hosting.match(/aws|amazon/gi)) {
+            return $scope.domainname = 'aws';
+        }
+        else ($scope.domainname = 'localhost');
 
-    this.isOpen = false;
-    this.hover = false;
-    var isOpenRight;
+        //TODO: sideNav controlling
+        //screen size variable
+        $scope.screenSize = $scope.$watch(function () {
+            return $mdMedia('(min-width: 1610px)');
+        }, function (trueOrfalse) {
+            return $scope.screenSize = trueOrfalse;
+        })
 
-    $scope.buttonSidenav = $scope.$watch(
-        function () {
-            return $scope.screenSize;
-        },
-        function (openOrNot) {
-            $scope.buttonSidenav = openOrNot;
-        });
-    $scope.openSidenav = function () {
-        $mdSidenav('right').open().then(function (isOpenRight = true) {
-            $log.debug("close RIGHT is done");
-            console.log("true isOpenRight", isOpenRight);
-        });
-    };
-    $scope.closeSidenav = function () {
-        $mdSidenav('right').close().then(function (isOpenRight = false) {
-            $log.debug("close RIGHT is done");
-            console.log("false isOpenRight", isOpenRight);
-            //$scope.isOpenRight = false;
-        });
-    };
+        this.isOpen = false;
+        this.hover = false;
+        var isOpenRight;
 
-    //TODO: Read Json data
-    var init = function () {
-        var hostDataUrl = '/jsonfiles/hosting.json';
-        getData.get(hostDataUrl).then(function successCallback(response) {
-            $scope.hostData = response.data;
-            console.log(response.data.hostProviders[0]);
-        }), function (response) {
-            defer.reject('could not find someFile.json');
-            console.log(response.data);
+        $scope.buttonSidenav = $scope.$watch(
+            function () {
+                return $scope.screenSize;
+            },
+            function (openOrNot) {
+                $scope.buttonSidenav = openOrNot;
+            });
+        $scope.openSidenav = function () {
+            $mdSidenav('right').open().then(function (isOpenRight = true) {
+                $log.debug("close RIGHT is done");
+                console.log("true isOpenRight", isOpenRight);
+            });
         };
-    };
-    init();
-});
+        $scope.closeSidenav = function () {
+            $mdSidenav('right').close().then(function (isOpenRight = false) {
+                $log.debug("close RIGHT is done");
+                console.log("false isOpenRight", isOpenRight);
+                //$scope.isOpenRight = false;
+            });
+        };
 
-//TODO run the slide show
-app.directive('tabsAutoplay', function ($interval) {
-    return {
-        restrict: 'A,E',
-        require: 'mdTabs',
-        link: function (scope, elm, attrs, mdTabsCtrl) {
-            var defaultDelay = 2000,
-                tabsAutoplayDelay = +attrs.tabsAutoplay || defaultDelay;
-
-            var tabsAutoplayInterval = $interval(function () {
-                var nextTab;
-                for (i = mdTabsCtrl.selectedIndex + 1; i <= mdTabsCtrl.tabs.length; i++) {
-                    nextTab = mdTabsCtrl.tabs[i];
-                    if (nextTab && (nextTab.scope.disabled !== true)) {
-                        return mdTabsCtrl.select(nextTab.getIndex());
-                    };
-                };
-                return mdTabsCtrl.select(0);
-            }, tabsAutoplayDelay);
-
-            var cleanup = function () {
-                $interval.cancel(tabsAutoplayInterval);
-                elm.off('click', cleanup);
+        //TODO: Read Json data
+        var init = function () {
+            var hostDataUrl = '/jsonfiles/hosting.json';
+            getData.get(hostDataUrl).then(function successCallback(response) {
+                $scope.hostData = response.data;
+                console.log(response.data);
+            }), function (response) {
+                //defer.reject('could not find someFile.json');
+                console.log(response.data);
             };
-            elm.on('click', cleanup);
-            scope.$on('$destroy', cleanup);
-        },
-    }
-});
+        };
+        init();
+        $scope.$on('$viewContentLoaded', function () {
+            var hostDataUrl = '/jsonfiles/hosting.json';
+            getData.get(hostDataUrl).then(function successCallback(response) {
+                $scope.hostData = response.data;
+                console.log(response.data.hostProviders[0]);
+            }), function (response) {
+                //defer.reject('could not find someFile.json');
+                console.log(response.data);
+            };
+            console.log("content loaded", response.data);
+        });
+    });
+
+    //TODO run the slide show
+    app.directive('tabsAutoplay', function ($interval) {
+        return {
+            restrict: 'A,E',
+            require: 'mdTabs',
+            link: function (scope, elm, attrs, mdTabsCtrl) {
+                var defaultDelay = 2000,
+                    tabsAutoplayDelay = +attrs.tabsAutoplay || defaultDelay;
+
+                var tabsAutoplayInterval = $interval(function () {
+                    var nextTab;
+                    for (i = mdTabsCtrl.selectedIndex + 1; i <= mdTabsCtrl.tabs.length; i++) {
+                        nextTab = mdTabsCtrl.tabs[i];
+                        if (nextTab && (nextTab.scope.disabled !== true)) {
+                            return mdTabsCtrl.select(nextTab.getIndex());
+                        };
+                    };
+                    return mdTabsCtrl.select(0);
+                }, tabsAutoplayDelay);
+
+                var cleanup = function () {
+                    $interval.cancel(tabsAutoplayInterval);
+                    elm.off('click', cleanup);
+                };
+                elm.on('click', cleanup);
+                scope.$on('$destroy', cleanup);
+            },
+        }
+    });
+
+})();
+
