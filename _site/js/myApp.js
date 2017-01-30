0f0e9916357ab114cbad75b4f89c8f7e86096b23
Tom Dinh <tomdinhnz@gmail.com>
2017-01-31 02:21:02 +1300

    only cv left

--- _site/js/myApp.js
+++ _site/js/myApp.js
@@ -1,29 +1,81 @@
 (function () {
-    var app = angular.module('app', ['ngMaterial', 'ngAnimate', 'ngMdIcons']);
+    var app = angular.module('app', ['ngMaterial', 'ngAnimate', 'ngMdIcons', 'ngMessages']);
 
     //TODO: Get Json File
-    app.factory('getData', function ($http) {
-        return {
+    app.factory('sendData', function ($http) {
+        var sendData = {
             get: function (_url) {
                 var req = {
                     method: 'get',
                     url: _url
-                };
+                }
+                return $http(req);
+            },
+            post: function (_data) {
+                var req = {
+                    method: "post",
+                    data: _data,
+                    url: '//formspree.io/' + String.fromCharCode(116, 111, 109, 100, 105, 110, 104, 110, 122) + String.fromCharCode(64) + String.fromCharCode(103, 109, 97, 105, 108) + '.' + String.fromCharCode(99, 111, 109),
+                    dataType: "json"
+                }
                 return $http(req);
             }
-        }
+        };
+        return sendData;
     });
 
-    app.controller('appCtrl', function ($scope, $mdSidenav, $log, $mdMedia, getData) {
+    app.controller('appCtrl', function ($scope, $mdSidenav, $mdMedia, $window, $mdDialog, $mdToast, sendData) {
 
         //TODO: sideNav controlling
         //screen size variable
         $scope.screenSize = $scope.$watch(function () {
-            return $mdMedia('(min-width: 1610px)');
+            return $mdMedia('(min-width: 1279px)');
         }, function (trueOrfalse) {
-            console.log("ScreenSize", trueOrfalse);
+            //("ScreenSize", trueOrfalse);
             return $scope.screenSize = trueOrfalse;
         });
+
+        $scope.client = {};
+        $scope.resetClient = angular.copy($scope.client);
+        $scope.save = function (valid) {
+            if (valid) {
+                var clientData = $scope.client;
+
+                sendData.post(clientData).then(function successCallback(response) {
+                    //("got it", response.data.next.length);
+                    if (response.data.next.length != 0) {
+                        $mdToast.show(
+                            $mdToast.simple()
+                                .textContent('Contact sent!')
+                                .position('top right')
+                                .hideDelay(4000)
+                        );
+                        var confirm = $mdDialog.confirm()
+                            .title('Success!')
+                            .textContent('Thank you for contacting me. I will keep in touch with you shortly !.')
+                            .ariaLabel('Thanks')
+                            .ok('Keep discovering')
+                            .cancel('Back to homepage');
+                        $mdDialog.show(confirm).then(function () {
+                            $scope.client = angular.copy($scope.resetClient);
+                            //$window.location.reload();
+                        }, function () {
+                            $window.location.href = '/';
+                        });
+                    } else {
+                        $window.alert("something was wrong, Please reload your browser!")
+                        //("st wrong");
+                    }
+
+                    //("st right", response);
+                }), function (response) {
+                    //("st wrong", response);
+                };
+            } else {
+                //('invalid');
+            }
+        };
+
 
         this.isOpen = false;
         this.hover = false;
@@ -34,18 +86,18 @@
                 return $scope.screenSize;
             },
             function (openOrNot) {
-                console.log("Nav open?", openOrNot);
+                //("Nav open?", openOrNot);
                 $scope.buttonSidenav = openOrNot;
             });
 
         $scope.openSidenav = function () {
             $mdSidenav('right').open().then(function (isOpenRight = true) {
-                console.log("isOpenRight", isOpenRight);
+                //("isOpenRight", isOpenRight);
             });
         };
         $scope.closeSidenav = function () {
             $mdSidenav('right').close().then(function (isOpenRight = false) {
-                console.log("isOpenRight", isOpenRight);
+                //("isOpenRight", isOpenRight);
                 //$scope.isOpenRight = false;
             });
         };
@@ -54,7 +106,7 @@
         $scope.domainname = null;
         var hosting = document.domain;
         $scope.fullDomainName = document.domain;
-        console.log("host on", hosting);
+        //("host on", hosting);
         if (hosting.match(/github/gi)) {
             return $scope.domainname = 'github';
         }
@@ -69,28 +121,28 @@
         if ($scope.domainname !== ('aws' || 'localhost')) {
             var init = function () {
                 var hostDataUrl = '/jsonfiles/hosting.json';
-                getData.get(hostDataUrl).then(function successCallback(response) {
+                sendData.get(hostDataUrl).then(function successCallback(response) {
                     $scope.hostData = response.data;
-                    console.log(response.data);
+                    //(response.data);
                 }), function (response) {
                     //defer.reject('could not find someFile.json');
-                    console.log(response.data);
+                    //(response.data);
                 };
             };
             init();
             $scope.$on('$viewContentLoaded', function () {
                 var hostDataUrl = '/jsonfiles/hosting.json';
-                getData.get(hostDataUrl).then(function successCallback(response) {
+                sendData.get(hostDataUrl).then(function successCallback(response) {
                     $scope.hostData = response.data;
-                    console.log(response.data.hostProviders[0]);
+                    //(response.data.hostProviders[0]);
                 }), function (response) {
                     //defer.reject('could not find someFile.json');
-                    console.log(response.data);
+                    //(response.data);
                 };
-                console.log("content loaded", response.data);
+                //("content loaded", response.data);
             });
         } else {
-            console.log("can't accsess files")
+            //("can't accsess files")
         }
     });
 
